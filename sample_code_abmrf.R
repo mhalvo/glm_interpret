f5565357cb2e40f68895a76069bb26c4072d13ba
mhalvo <36017085+mhalvo@users.noreply.github.com>
2020-05-07 15:13:48 -0700

    Update all files based on revision

---
+++ sample_code_abmrf.R
@@ -0,0 +1,146 @@
+#########################################
+# sample_code_abmrf.R                   #
+#    Code used to generate figures      #
+#     and table estimates for empirical #
+#     paper example                     #
+#########################################
+# Max Halvorson #
+#   2020-05-07  #
+#################
+
+#######################
+# Setup and data load #
+#######################
+rm(list=ls())
+library('MASS')
+library('ggplot2')
+library('psych')
+options(max.print=2000, scipen=7, tibble.print_min=70)
+
+setwd('C:/Users/Max Halvorson/Dropbox/GLM Main Effects Paper')
+
+dat = read.table("C:/Users/Max Halvorson/Dropbox/GLM Main Effects Paper/ABMRF Data/ABMRF_Clean.txt", header = T)
+dat$gen <- dat$gen - 1
+dat$purg_new <- dat$purg - 1
+
+
+##################################
+# Analysis for graphs and tables #
+##################################
+### Create triplets (2.5th percentile, point estimate, 97.5th percentile)
+# Step 1a: fit model
+pmod <- glm(negpysum ~ as.factor(gen) + purg_new + pre + ss, 
+            family=poisson(link="log"),
+            data = dat)
+summary(pmod)
+# Step 1b: extract estimates for tables, betas, variance-covariance matrix
+exp(coef(pmod))                # ORs, IRRs for table
+exp(confint(pmod, level=.95))  # OR, IRR CIs for table
+p_pes <- coef(pmod)
+p_vcov <- vcov(pmod)
+
+# Step 2a: set up counterfactuals (x values at which to probe model)
+purgrange <- seq(0, 3, by=.25) # hypothetical purg levels
+scenlen <- length(purgrange)   # number of scenarios per levels of other predictors
+desc <- psych::describe(dat)
+gen_m <- 0
+gen_f <- 1
+pre_m <- desc$mean[7]
+ss_m <- desc$mean[9]
+# male
+df_male <- data.frame(int = 1,
+                      gen = rep(gen_m, scenlen),
+                      purg_new = purgrange,
+                      pre = rep(pre_m, scenlen),
+                      ss = rep(ss_m, scenlen))
+# female
+df_female <- data.frame(int = 1,
+                        gen = rep(gen_f, scenlen),
+                        purg_new = purgrange,
+                        pre = rep(pre_m, scenlen),
+                        ss = rep(ss_m, scenlen))
+names(df_male)[1] <- names(df_female)[1] <- "(Intercept)"
+
+# Step 2b: generate expected values from our data model 
+pred_male <- predict(pmod, newdata=df_male, type="response")
+pred_female <- predict(pmod, newdata=df_female, type="response")
+
+### Step 3a: simulate a range of plausible models based on distribution of parameters from model fit 
+sims <- 10000
+simparams <- mvrnorm(sims,p_pes,p_vcov)
+
+# Step 3b: create triplets for each set of scenarios
+matdfm <- as.matrix(df_male)
+matdff <- as.matrix(df_female)
+lo <- pe <- hi <- rep(NA,scenlen)
+
+tripm <- data.frame(lo=lo, pe=pred_male, hi=hi) # create triplets for male
+for(i in 1:scenlen){
+  simmu <- simparams %*% matdfm[i,]    # result of Xbeta for count
+  simy <- exp(simmu)                  # inverse of log-link - convert to a count
+  
+  tripm$lo[i] <- quantile(simy,.025)
+  tripm$hi[i] <- quantile(simy,.975)
+}
+
+tripf <- data.frame(lo=lo, pe=pred_female, hi=hi) # create triplets for female
+for(i in 1:scenlen){ 
+  simmu <- simparams %*% matdff[i,]  
+  simy <- exp(simmu)
+  
+  tripf$lo[i] <- quantile(simy,.025)
+  tripf$hi[i] <- quantile(simy,.975)
+}
+
+dfplotm <- cbind(matdfm, tripm)
+dfplotf <- cbind(matdff, tripf)
+dfplot <- rbind(dfplotm, dfplotf)
+dfplot$gen <- as.factor(dfplot$gen)
+levels(dfplot$gen) <- c("Male","Female")
+
+##############################
+# Create graphics and tables #
+##############################
+### Plot triplets as fit line plus region of uncertainty
+(ex2p <- ggplot(data=dfplot, mapping=aes(x=purg_new, y=pe, linetype=gen)) +
+    geom_line() +
+    geom_ribbon(aes(ymin=lo, ymax=hi), alpha=.10) +
+    labs(linetype="Gender") +
+    xlab("Positive Urgency") +
+    ylab("Expected Count of Alcohol Consequences") +
+    ylim(0, 17) +
+    scale_x_continuous(breaks=c(0,1,2,3),
+                       labels=c("1 - Disagree\n Strongly","2 - Disagree\n Somewhat",
+                                "3 - Agree\n Somewhat","4 - Agree\n Strongly")) +
+    theme_bw()
+)  
+h <- 6
+ggsave("Images/alcons_by_gender.png",
+       height=h, 
+       width=h*1.619)
+
+### Calculate predicted values for tables
+dftab <- data.frame(int=rep(1,8),
+                    gen=rep(c(0, 1), times=4),
+                    purg_new=rep(0:3, each=2),
+                    pre=pre_m,
+                    ss=ss_m)
+dftab$yev <- predict(pmod, newdata=dftab, type="response") # point estimates
+dftab
+
+### Calculate 95% confidence intervals for individual point estimates
+names(dftab)[1] <-"(Intercept)"
+dftabm <- as.matrix(dftab[,-6])
+simmu <- simparams%*%t(dftabm) 
+simy <- exp(simmu)
+
+dftab$lo <- apply(simy, 2, quantile, .025) 
+dftab$hi <- apply(simy, 2, quantile, .975)
+
+### Have R output these values
+paste0("Gender = ", dftab$gen,
+       "; Pos Urg = ", round(dftab$purg_new, 2),
+       "; Planning = ", round(dftab$pre, 2),
+       "; Sen Seek = ", round(dftab$ss, 2),
+       "; Predicted count: ", round(dftab$yev, 2), 
+       " [",round(dftab$lo, 2), ", ", round(dftab$hi, 2), "]")
--- sample_code_generic.R
+++ sample_code_generic.R
@@ -6,38 +6,37 @@
 #     for individual use cases           #
 ##########################################
 # Max Halvorson #
-#   2020-03-06  #
+#   2020-05-07  #
 #################
 
-rm(list=ls()) 
-library('MASS')       # for mvrnorm
-library('tidyverse')  # for ggplot
-library('psych')      # for describe
+rm(list=ls())
+library('MASS')
+library('ggplot2')
 options(max.print=2000, scipen=7, tibble.print_min=70)
 
-setwd('C:/Users/Max Halvorson/Documents/GitHub/glm_main_effects')
+setwd('C:/Users/Max Halvorson/Dropbox/GLM Main Effects Paper')
 
 ##########################
 # Simulated data example #
 ##########################
 ### Simulate data
 set.seed(1337) 
-b0 <- 1        # Intercept
-b1 <- -1       # X1 Effect
+b0 <- 0        # Intercept
+b1 <- -2       # X1 Effect
 b2 <- 1        # X2 Effect
 n <- 500       # Sample Size
 mu <- rep(0,2) # Specify means
 S <- matrix(c(  1,-.1,
-              -.1,  1),nrow=2,ncol=2) # Specify covariance matrix
-sigma <- 1                            # Level 1 error
+              -.1,  1),nrow=2,ncol=2)   # Specify covariance matrix
+sigma <- 1                              # Level 1 error
 rawvars <- mvrnorm(n=n, mu=mu, Sigma=S) # Simulate predictors from a multivariate normal distribution
 
 id <- seq(1:n)
-eij <- rep(rnorm(id, 0, sigma)) # Add normally distributed error
+eij <- rep(rnorm(id, 0, sigma))         # Add normally distributed error
 xb <- (b0) + (b1)*rawvars[,1] + (b2)*rawvars[,2] + eij
 
 pr <- 1 / (1 + exp(-xb))        # pass through logistic function to convert xb to probabilities
-y <- rbinom(n, 1, pr)       # bernoulli response variable
+y <- rbinom(n, 1, pr)           # bernoulli response variable
 df <- data.frame(id=id,
                  y=y,
                  x1=rawvars[,1],
@@ -47,19 +46,19 @@ df <- data.frame(id=id,
 # Analysis for graphs and tables #
 ##################################
 ##### Create triplets (2.5th percentile, point estimate, 97.5th percentile)
-### Step 1: fit model
+### Step 1a: fit model
 logmod <- glm(y ~ x1 + x2, 
               data=df, 
               family="binomial")
 
-### Step 2: extract estimates for tables, betas, variance-covariance matrix
+### Step 1b: extract estimates for tables, betas, variance-covariance matrix
 summary(logmod)                 # Betas for table
 exp(logmod$coefficients)        # ORs for table
 exp(confint(logmod, level=.95)) # OR CIs for table
 pes <- coef(logmod)             
 vcov <- vcov(logmod)  
 
-### Step 3: set up counterfactuals (x values at which to probe model)
+### Step 2a: set up counterfactuals (x values at which to probe model)
 x1range <- seq(-3, 3, by=.1) # range of hypothetical x1 values
 scenlen <- length(x1range)   # number of x1 scenarios per level of x2
 xi1 <- xi2 <- as.data.frame(matrix(NA,
@@ -73,60 +72,79 @@ xi2$x2 <- rep(1, scenlen)
 matxi1 <- as.matrix(xi1)
 matxi2 <- as.matrix(xi2)
 
-### Step 4: simulate many plausible models based on distribution of parameters from model fit in Step 1 
+### Step 2b: generate predictions from the model we fit from our data
+pred1 <- predict(logmod, newdata=xi1, type="response")
+pred2 <- predict(logmod, newdata=xi2, type="response")
+
+### Step 3a: simulate a range of plausible models based on distribution of parameters from model fit 
 sims <- 10000
 simparams <- mvrnorm(sims,pes,vcov)
 
-### Step 5: create triplets for each set of scenarios
+### Step 3b: create triplets for each set of scenarios
 lo <- pe <- hi <- rep(NA,scenlen)
 # for x2=low
-trip1 <- data.frame(lo=lo, pe=pe, hi=hi) # create triplets for x2=low
+trip1 <- data.frame(lo=lo, pe=pred1, hi=hi) # create triplets for x2=low
 for(i in 1:nrow(matxi1)){ 
   simmu <- simparams%*%matxi1[i,]  # result of XBeta for all simulated models
   simy <- 1/(1 + exp(-simmu))      # logistic function - convert result to a probability
+#  simy <- exp(simmu)              # exponential function - inverse of log-link, uncomment if Poisson
   simy <- sort(simy)
   
   trip1$lo[i] <- quantile(simy,.025)
-  trip1$pe[i] <- quantile(simy,.50)
   trip1$hi[i] <- quantile(simy,.975)
 }
-trip2 <- data.frame(lo=lo, pe=pe, hi=hi) # create triplets for x2=high
+trip2 <- data.frame(lo=lo, pe=pred2, hi=hi) # create triplets for x2=high
 for(i in 1:nrow(matxi2)){ 
   simmu <- simparams%*%matxi2[i,] 
   simy <- 1/(1 + exp(-simmu)) 
   simy <- sort(simy)
   
   trip2$lo[i] <- quantile(simy,.025)
-  trip2$pe[i] <- quantile(simy,.50)
   trip2$hi[i] <- quantile(simy,.975)
 }
 dfplot1 <- data.frame(x1=xi1$x1, x2=xi1$x2, 
                       pe=trip1$pe, lo=trip1$lo, hi=trip1$hi) # attach triplets to predictors
-
 dfplot2 <- data.frame(x1=xi2$x1, x2=xi2$x2, 
                       pe=trip2$pe, lo=trip2$lo, hi=trip2$hi)
 dfplot <- rbind(dfplot1, dfplot2) # create a single plotting dataset
 dfplot$x2 <- as.factor(dfplot$x2)
 levels(dfplot$x2) <- c("High (+1 SD)","Low (-1 SD)") 
 
-##### Create graphics and  tables
-### Step 6: plot triplets as fit line plus region of uncertainty
+##############################
+# Create graphics and tables #
+##############################
+### Plot triplets as fit line plus region of uncertainty
 (ex1p <- ggplot(data=dfplot, mapping=aes(x=x1, y=pe, linetype=x2)) +
     geom_ribbon(aes(ymin=lo, ymax=hi), alpha=.10) +
     geom_line() +
-    labs(linetype="X2") +
-    xlab("X1") +
+    labs(linetype=expression(x[2])) +
+    xlab(expression(x[1])) +
     ylab("Expected Probability") +
     scale_y_continuous(breaks=seq(0, 1, .2), minor_breaks=seq(0, 1, .1), limits=c(0, 1)) +
     theme_bw()
 )
 h <- 6
-ggsave(file="p_by_x1x2.png",
+ggsave(file="Images/p_by_x1x2.png", 
        height=h,
        width=h*1.619)
 
-### Step 7: calculate values for tables
-dftab <- data.frame(x1=rep(c(-1, 0, 1), each=3), 
+### Calculate predicted values for tables
+dftab <- data.frame(x0=rep(1, times=9),
+                    x1=rep(c(-1, 0, 1), each=3), 
                     x2=rep(c(-1, 0, 1), times=3))
-dftab$yev <- predict(logmod, newdata=dftab, type="response") # plug-and-chug new predictors through model
+dftab$yev <- predict(logmod, newdata=dftab, type="response") # point estimates
 dftab
+
+### Calculate 95% confidence intervals for individual point estimates
+names(dftab)[1] <-"(Intercept)"
+simmu <- simparams%*%t(as.matrix(dftab)[,1:3]) 
+simy <- 1/(1 + exp(-simmu))
+
+dftab$lo <- apply(simy, 2, quantile, .025) 
+dftab$hi <- apply(simy, 2, quantile, .975)
+
+### Have R output these values
+paste0("x1 = ", dftab$x1,
+       "; x2 = ", dftab$x2,
+       "; Predicted probability: ", round(dftab$yev, 2), 
+       " [",round(dftab$lo, 2), ", ", round(dftab$hi, 2), "]")
